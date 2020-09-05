0efe90dfa86b873630e0f82860579d4334c52e47
mishamyrt <misha@myrt.co>
2020-09-05 04:00:28 +0300

    🥦🔥

--- helpers/apply-patches.bash
+++ helpers/apply-patches.bash
@@ -26,17 +26,13 @@ function apply_patches() {
         continue
       fi
       if git apply --check "$patch"; then
-        echo -e "\033[01;32m"
         git am "$patch"
-        echo -e "\033[0m"
       elif patch -f -p1 --dry-run <"$patch" >/dev/null; then
         #This will fail
-        echo -e "\033[32m"
         git am "$patch" || true
         patch -f -p1 <"$patch"
         git add -u
         git am --continue
-        echo -e "\033[0m"
       else
         echo -e "\033[01;31m\n Failed applying $patch ... 033[0m"
       fi
