fa69deef24a6b45b150d5de537e98d4a6f9666f0
mishamyrt <misha@myrt.co>
2020-09-12 14:46:43 +0300

    envsetup: Fix broken path

--- envsetup.bash
+++ envsetup.bash
@@ -1,3 +1,3 @@
 # shellcheck source=helpers/config.bash disable=SC2139
-source "$ppe_scripts_path/helpers/config.bash"
+source ./pancake/helpers/config.bash
 alias pk="$ppe_scripts_path/script.bash"
